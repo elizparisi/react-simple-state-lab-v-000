26370488d26521824a1dac949e1dc57d578702e1
Liz P <parisielizabeth@gmail.com>
2021-01-14 04:46:58 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,7 +1,6 @@
+import React from 'react'
 
-import React, {Component} from 'react';
-
-export default class Cell extends Component {
+export default class Cell extends React.Component {
     constructor(props){
         super(props)
         this.state = {
