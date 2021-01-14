c9fcf795483b37d610768682d44af79fda42f94e
Liz P <parisielizabeth@gmail.com>
2021-01-14 04:45:02 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -15,11 +15,7 @@ export default class Cell extends React.Component {
 
     render(){
         return(
-            <div className='cell'
-             style={{backgroundColor: this.state.color}}
-             onClick={this.handleClick}>
-
-             </div>
+            <div className="cell" onClick={this.handleClick} style={{backgroundColor: this.state.color}}> </div>
         )
     }
 }
